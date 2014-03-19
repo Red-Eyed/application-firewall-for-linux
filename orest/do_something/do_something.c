663f317320b6b06821032d9ace370b178d6a5e67
vasnet515@gmail.com <vasnet515@gmail.com@019164a8-2d48-a202-41b5-38261d74085d>
2014-03-19 16:21:23 +0000

    Cписок процесов

--- orest/do_something/do_something.c
+++ orest/do_something/do_something.c
@@ -2,18 +2,20 @@
 
 static int __init do_something_load(void)
 {
-	ds_param_init();	
-	SUCCESS=ds_char_test_dev_init();
-	SUCCESS=ds_proc_init();
-	printk(KERN_INFO "do_something load\n");
-	return SUCCESS;
+    ds_param_init();
+    SUCCESS=ds_char_test_dev_init();
+    SUCCESS=ds_proc_init();
+    //SUCCESS=ds_process_init();
+    printk(KERN_INFO "do_something load\n");
+    return SUCCESS;
 }
 
 static void __exit do_something_unload(void)
 {
-	ds_char_test_dev_exit();
-	ds_proc_exit();
-	printk(KERN_INFO "do_something unload\n");
+    ds_char_test_dev_exit();
+    ds_proc_exit();
+    //ds_process_exit();
+    printk(KERN_INFO "do_something unload\n");
 }
 
 module_init(do_something_load);
