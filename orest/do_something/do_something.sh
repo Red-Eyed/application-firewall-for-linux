39b046c64c31c92ecc7e5d34544cc8c245e8a776
vasnet515@gmail.com <vasnet515@gmail.com@019164a8-2d48-a202-41b5-38261d74085d>
2014-03-19 16:30:59 +0000

    обновление

--- orest/do_something/do_something.sh
+++ orest/do_something/do_something.sh
@@ -2,7 +2,7 @@
 #echo "56367"
 make
 insmod do_something.ko
-#rmmod do_something
+rmmod do_something
 #insmod do_something.ko ds_mod_param=7
 #rmmod do_something
 dmesg |tail
--- orest/do_something/ds_proc.h
+++ orest/do_something/ds_proc.h
@@ -4,36 +4,36 @@ char *msg_proc;
 int read_proc(struct file *filp,char *buf,size_t count,loff_t *offp );
 
 struct file_operations proc_fops = {
-.read= read_proc
+    .read= read_proc
 };
 
 int ds_proc_init(void)
 {
-  proc_create("hello_proc",0,NULL,&proc_fops);
-  msg_proc=" Hello World_proc ";
-  len_proc=strlen(msg_proc);
-  temp_proc=len_proc;
-  printk(KERN_INFO "1.len=%d",len_proc);
-  
-  return SUCCESS;
+    proc_create("hello_proc",0,NULL,&proc_fops);
+    msg_proc=" Hello World_proc ";
+    len_proc=strlen(msg_proc);
+    temp_proc=len_proc;
+    printk(KERN_INFO "1.len=%d",len_proc);
+
+    return SUCCESS;
 }
 
 void ds_proc_exit(void)
 {
-  remove_proc_entry("hello_proc",NULL);
+    remove_proc_entry("hello_proc",NULL);
 
 }
 int read_proc(struct file *filp,char *buf,size_t count,loff_t *offp ) 
 {
 
-if(count>temp_proc)
-{
-count=temp_proc;
-}
-temp_proc=temp_proc-count;
-copy_to_user(buf,msg_proc, count);
-if(count==0)
-temp_proc=len_proc;
-   
-return count;
+    if(count>temp_proc)
+    {
+        count=temp_proc;
+    }
+    temp_proc=temp_proc-count;
+    copy_to_user(buf,msg_proc, count);
+    if(count==0)
+        temp_proc=len_proc;
+
+    return count;
 }
