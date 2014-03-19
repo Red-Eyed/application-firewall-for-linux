1a40116299c47f5f78bdb6fbd1320b2c7850585e
vasnet515@gmail.com <vasnet515@gmail.com@019164a8-2d48-a202-41b5-38261d74085d>
2014-03-19 16:30:59 +0000

    обновление

--- trunk/orest/do_something/do_something.h
+++ trunk/orest/do_something/do_something.h
@@ -2,14 +2,22 @@ static int SUCCESS=0;
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/kernel.h>
-#include<linux/init.h>
-#include <linux/stat.h>
-#include <linux/fs.h>
+#include <linux/init.h>
+//#include <linux/stat.h>
+//#include <linux/fs.h>
 #include <linux/proc_fs.h>
 #include <asm-generic/uaccess.h>
 #include <linux/string.h>
 #include <linux/vmalloc.h>
+#include <linux/types.h>
+#include <linux/signal.h>
+//#include <linux/syscalls.h>
+#include <linux/sched.h>
+//#include <linux/tty.h>
+#include <linux/version.h>
+#include <linux/kthread.h>
 #include "ds_param.h"
 #include "info.h"
 #include "char_test_dev.h"
 #include "ds_proc.h"
+//#include "ds_process.h"
--- trunk/orest/do_something/ds_param.h
+++ trunk/orest/do_something/ds_param.h
@@ -4,6 +4,6 @@ MODULE_PARM_DESC(ds_mod_param, "An integer");
 
 void ds_param_init(void)
 {
-  printk(KERN_INFO "do_something load\n");
-  printk(KERN_INFO "ds_mod_param=%d\n",ds_mod_param);
+    printk(KERN_INFO "do_something load\n");
+    printk(KERN_INFO "ds_mod_param=%d\n",ds_mod_param);
 }
